dce075341a7b436add43aed1bb99a28c8c02cd48
SilentCoding1 <silent9573@gmail.com>
2023-11-24 00:28:29 -0500

    Add Ruby Item

--- src/main/java/net/silent/examplemod/ExampleMod.java
+++ src/main/java/net/silent/examplemod/ExampleMod.java
@@ -2,6 +2,7 @@ package net.silent.examplemod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.silent.examplemod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class ExampleMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
