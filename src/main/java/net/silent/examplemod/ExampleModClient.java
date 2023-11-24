e1adab60435c3b0d725e7ab68ae667d893b051c5
SilentCoding1 <silent9573@gmail.com>
2023-11-23 23:34:32 -0500

    Initial commit

---
+++ src/main/java/net/silent/examplemod/ExampleModClient.java
@@ -0,0 +1,10 @@
+package net.silent.examplemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExampleModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
